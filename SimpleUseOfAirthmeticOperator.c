c9d0b5171bf56c4950119a93f90a8035090c2062
Ayush Arya <84086568+Ayush888-coder@users.noreply.github.com>
2021-05-25 09:43:58 +0530

    Create SimpleUseOfAirthmeticOperator.c

---
+++ SimpleUseOfAirthmeticOperator.c
@@ -0,0 +1,19 @@
+  
+#include <stdio.h>
+int main() 
+{
+   int a=35;
+   int b=34;
+
+   printf("sub=%d\n",a-b);
+   
+      printf("sum=%d\n,a+b");
+      
+        printf("multiply=%d\n",a*b); 
+		 printf("divide=%d\n",a/b); 
+		  printf("remainder=%d\n",a%b);
+   
+
+ 
+   return 0;
+}
